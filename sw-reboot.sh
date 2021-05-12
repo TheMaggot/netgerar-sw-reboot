d945b731f9d8c419ceaa6a2046b9e0a2a461e99d
Marco Wenzel <foren@der-wenz.de>
2021-05-12 22:00:05 +0200

    Add some comments

--- sw-reboot.sh
+++ sw-reboot.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# encrypt password by follwing command: 
 # password=$(echo "this is a secret." | openssl enc -e -des3 -base64 -pass pass:mypasswd -pbkdf2)
+
+# command line arguments
 host=$1
 password=$2
 
 if [[ $# -ne 2 ]];
 then
     echo "usage:"
-    echo "sw-reset.sh <HOST> <ENCRYPTED_PASSWORD>"
+    echo "./sw-reboot.sh <HOST> <ENCRYPTED_PASSWORD>"
     exit -1
 fi
 
+# place wher to store the cookie
 cookie="/tmp/cookie.txt"
 
+# netgear method of encrypting the password before sending it via POST to the webserver
 function netgear_encrypt () {
     str1=$1
     str2=$2
@@ -37,6 +42,7 @@ function netgear_encrypt () {
     echo $result
 }
 
+# grep value from HTML tag
 function get_htmlvalue () {
     site=$1
     tag=$2
@@ -49,14 +55,22 @@ function get_htmlvalue () {
     echo $value
 }
 
+# decrypt password argument
 pw_decr=`echo "${password}" | openssl enc -d -des3 -base64 -pass pass:mypasswd -pbkdf2`
+# get rand value which is used to encrypt the password in netgear way
 rand=$(get_htmlvalue "login.cgi" "rand")
+# encrypt password
 pw_ng_encr=$(netgear_encrypt $pw_decr $rand)
+# login
 curl -s --data-raw "password=$pw_ng_encr" -c $cookie http://$host/login.cgi > /dev/null
 
+# get hash which is used to confirm reboot action
 hash=$(get_htmlvalue "device_reboot.cgi" "hash")
+# if .cgi is not available, try .htm
 [[ -z "$hash" ]] && hash=$(get_htmlvalue "device_reboot.htm" "hash")
 
+# let's go reboot
 curl -s  --data-raw "CBox=on&hash=$hash" -b $cookie http://$host/device_reboot.cgi > /dev/null
 
+# clean up
 rm $cookie
